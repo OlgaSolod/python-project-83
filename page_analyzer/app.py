e09659418d7851f3bab4f38e397833949bd12235
Olga <osoldko@ntv.ru>
2024-10-08 22:54:23 +0300

    fix lint errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, url_for, get_flashed_messages, flash, request, redirect
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    flash,
+    request,
+    redirect
+    )
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
 import os
@@ -11,11 +18,7 @@ app = Flask(__name__)
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
-try:
-    conn = psycopg2.connect(DATABASE_URL)
-    print("Database connected")
-except:
-    print("Can't connect to database")
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route("/")
@@ -34,11 +37,14 @@ def urls_get():
         data=urls
         )
 
+
 @app.post("/urls")
 def urls_post():
     actual_url = request.form.get('url')
     if validate_url(actual_url):
-        url = f"{urlparse(actual_url).scheme}://{urlparse(actual_url).hostname}"
+        scheme = urlparse(actual_url).scheme
+        hostname = urlparse(actual_url).hostname
+        url = f"{scheme}://{hostname}"
         if check_url_in_db(url):
             url_id = insert_url_in_db(url)
             flash("Страница успешно добавлена", category='success')
@@ -52,24 +58,31 @@ def urls_post():
         flash("Некорректный URL", category='danger')
         return render_template('index.html'), 422
 
+
 @app.get('/urls/<int:id>')
 def url_page(id):
 
     data = get_data_by_id(id)
     if data:
         id, name, created_at = data
-        return render_template('url_page.html', name=name, id=id, created_at=created_at)
+        return render_template(
+            'url_page.html', name=name, id=id, created_at=created_at
+            )
     else:
         return render_template('404.html')
 
+
 def check_url_in_db(url):
-    sql = f"select * from urls where name = %s;"
+    sql = "select * from urls where name = %s;"
     with conn.cursor() as cur:
         cur.execute(sql, (url,))
-        return cur.fetchone() is None    
-    
+        return cur.fetchone() is None
+
+
 def insert_url_in_db(url):
-    sql = f"insert into urls (name) values (%s)  RETURNING id;"
+    sql = '''insert into urls
+            (name) values (%s)
+            RETURNING id;'''
     with conn.cursor() as cur:
         cur.execute(sql, (url,))
         url_id = cur.fetchone()[0]
@@ -78,19 +91,21 @@ def insert_url_in_db(url):
 
 
 def get_data_by_id(id):
-    sql = f"select id, name, created_at::date from urls where id = %s;"
+    sql = "select id, name, created_at::date from urls where id = %s;"
     with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
         cur.execute(sql, (id,))
         return cur.fetchone()
-    
+
+
 def get_all_urls():
-    sql = f"select id, name, created_at::date from urls;"
+    sql = "select id, name, created_at::date from urls;"
     with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
         cur.execute(sql)
         return cur.fetchall()
-    
+
+
 def find_id_by_name(name):
-    sql = f"select id from urls where name = %s;"
+    sql = "select id from urls where name = %s;"
     with conn.cursor() as cur:
         cur.execute(sql, (name,))
         return cur.fetchone()[0]
